8d1614c39dc44567dd3118573c1db5b00640899b
Matīss ApinisMatīss Apinisgit config --global user.email matiss.apinis@gmail.com <matissapinis@DESKTOP-S0S3HVV.localdomain>
2023-08-08 15:29:09 +0300

    Added initial homepage content

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # GET /about
+  ## GET /about page:
   get "about", to: "about#index"
 
+  ## GET root homepage page:
+  root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
